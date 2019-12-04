30a214d8d272fb07420af29152b9abc7c2d732de
goodhousekeeper <github@goodhousekeeper.ru>
2019-12-04 19:33:55 +0300

    add files

---
+++ index.js
@@ -0,0 +1,6 @@
+import * as delphi from './src/TApplication';
+module.exports.default = delphi.default;
+module.exports.TApplication = delphi.TApplication;
+module.exports.Panels = delphi.Panels;
+
+
