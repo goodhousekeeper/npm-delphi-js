7a094cc0e672fb4334b32f06a6f0bec4ee65f4f6
goodhousekeeper <github@goodhousekeeper.ru>
2019-12-09 21:50:29 +0300

    sync folder recreation

--- bin/post-install.js
+++ bin/post-install.js
@@ -1,16 +1,11 @@
+/* enable npm modules */
 const gentlyCopy = require('gently-copy');
-const fs = require('fs');
 const fse = require('fs-extra');
 const path = require('path');
-
+/* set path and source files  */
 const fileList = ['src', 'index.js'];
 const delphiFolder = path.join(process.env.INIT_CWD, 'delphi-js');
-
-
-fse.remove(delphiFolder, err => {
-    console.error(err)
-});
-
-fs.mkdirSync(delphiFolder);
-
+/* empty or create folder*/
+fse.emptyDirSync(delphiFolder);
+/* copy source files */
 gentlyCopy(fileList, delphiFolder);
