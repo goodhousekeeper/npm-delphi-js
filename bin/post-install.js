16c724502bee9f0577fa240d49b6463acaff9c0e
goodhousekeeper <github@goodhousekeeper.ru>
2019-12-20 15:07:10 +0300

    copy DOCUMENTATION.md and CHANGELOG.md

--- bin/post-install.js
+++ bin/post-install.js
@@ -3,7 +3,7 @@ const gentlyCopy = require('gently-copy');
 const fse = require('fs-extra');
 const path = require('path');
 /* set path and source files  */
-const fileList = ['src', 'index.js'];
+const fileList = ['src', 'index.js', 'DOCUMENTATION.md', 'CHANGELOG.md'];
 const delphiFolder = path.join(process.env.INIT_CWD, 'delphi-js');
 /* empty or create folder*/
 fse.emptyDirSync(delphiFolder);
